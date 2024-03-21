78e656f7341ed32ca1f488a7354018b96b0ba851
Igor Verkhov <iVerkhov@icoud.com>
2024-03-21 19:13:50 +0300

    Initial Commit

---
+++ HighRollers/HighRollersApp.swift
@@ -0,0 +1,17 @@
+//
+//  HighRollersApp.swift
+//  HighRollers
+//
+//  Created by Игорь Верхов on 21.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct HighRollersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
